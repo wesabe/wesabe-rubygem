6bfd7690580dd8c02141820092e27b0ef330b80e
Brian Donovan <brian.donovan@gmail.com>
2008-08-04 12:25:02 -0700

    Update the example to use the new interface and to print out a list of accounts.

---
+++ examples/list_accounts.rb
@@ -0,0 +1,35 @@
+require File.dirname(__FILE__) + '/../lib/wesabe'
+
+begin
+  require 'readline'
+  include Readline
+rescue LoadError
+  def readline(prompt=nil)
+    print prompt if prompt
+    gets
+  end
+end
+
+username = readline("Username: ")
+password = readline("Password: ")
+
+wesabe = Wesabe.new username, password
+
+# fetch your account data
+accounts = wesabe.accounts
+max_name = accounts.map{|a|a.name.size}.max
+max_balance = accounts.map{|a|a.balance.to_s.size}.max
+
+max_name = [max_name, "Account".size].max
+max_balance = [max_balance, "Balance".size].max
+
+# write header
+puts "%-#{max_name}s  %#{max_balance}s" % ["Account", "Balance"]
+puts "-" * (max_name + 2 + max_balance)
+
+# write data
+accounts.sort_by {|a| a.name}.each do |account|
+  name_formatter = "%-#{max_name}s"
+  balance_formatter = account.balance ? "%#{max_balance}.2f" : "%s"
+  puts "#{name_formatter}  #{balance_formatter}" % [account.name, account.balance]
+end
